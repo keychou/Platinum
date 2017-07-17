dfc24d6486d76c9f0e01b22579562d462cbf055c
klein <zhoukaihf@foxmail.com>
2017-07-17 19:26:18 +0800

    copy source files from Source/Devices/
    Signed-off-by: klein <zhoukaihf@foxmail.com>

--- Source/Platform/Android/module/platinum/jni/Android.mk
+++ Source/Platform/Android/module/platinum/jni/Android.mk
@@ -39,7 +39,15 @@ endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE     := platinum-jni
-LOCAL_SRC_FILES  := platinum-jni.cpp
+
+MY_CPP_PATH  := $(LOCAL_PATH)/
+My_All_Files := $(shell find $(MY_CPP_PATH)/.)
+My_All_Files := $(My_All_Files:$(MY_CPP_PATH)/./%=$(MY_CPP_PATH)%)
+MY_CPP_LIST  := $(filter %.cpp %.c,$(My_All_Files)) 
+MY_CPP_LIST  := $(MY_CPP_LIST:$(LOCAL_PATH)/%=%)
+
+LOCAL_SRC_FILES := $(MY_CPP_LIST)
+
 LOCAL_LDLIBS     += -llog
 LOCAL_LDLIBS     += -landroid
 
