4ec8358ce463434fa9031a1d4a95f8c37f25e237
klein <zhoukaihf@foxmail.com>
2017-07-18 16:21:52 +0800

    update build.sh
    Signed-off-by: klein <zhoukaihf@foxmail.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,27 @@
-echo "/-----------build project-------------/"
+echo "## STEP 1-----------build project-------------"
 scons target=arm-android-linux build_config=Release
+echo ""
+echo ""
+echo ""
 
-echo "/------------push bin to device-------/"
+echo "## STEP 2------------push bin to device-------"
 adb push Targets/arm-android-linux/Release/MicroMediaController system/bin
+echo ""
+echo ""
+echo ""
+
+
+echo "## STEP 3---------cd to Source/Platform/Android/module/platinum/jni/-----"
+cd Source/Platform/Android/module/platinum/jni/
+echo ""
+echo ""
+echo ""
+
+echo "## STEP 4-----------excute make.sh------------"
+./make.sh
+echo ""
+echo ""
+echo ""
+
+cd -
+
